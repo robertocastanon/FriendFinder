d24e7465ea6daf12d0d9a81165254d8af54c23f9
Roberto Castanon <robertocastanon925@gmail.com>
2019-05-26 13:09:51 -0700

    added routing for /survey and a defualt, also they route to existing index.html and survey.html pages

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ var app = express();
 
 var PORT = process.env.PORT || 4444;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+require('./app/routing/htmlRoutes')(app);
+
 app.listen(PORT, function() {
     console.log(`App listening on PORT ${PORT}`);
 })
