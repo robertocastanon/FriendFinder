020ac6a94c770ef424c06ea916d7abb5bed9f293
Roberto Castanon <robertocastanon925@gmail.com>
2019-05-26 12:34:40 -0700

    app.js now listens to port 4444

--- app.js
+++ app.js
@@ -2,3 +2,9 @@ const express = require('express');
 const path = require('path');
 
 var app = express();
+
+var PORT = process.env.PORT || 4444;
+
+app.listen(PORT, function() {
+    console.log(`App listening on PORT ${PORT}`);
+})
